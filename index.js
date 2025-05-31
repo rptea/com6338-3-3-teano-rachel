baf9f85b3ce10d50ecfb187fc97b67e2f85711f1
Rachel Teano <rp.teano@ufl.edu>
2025-05-30 21:04:52 -0400

    modified index.js, added questions and answers

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+var questionsArr = [
+    {
+        question: 'The genome of a house cat is 95.6 percent tiger',
+        answer: true
+    },
+    {
+        question: 'Many cats are lactose intolerant',
+        answer: true
+    },
+    {  
+        question: 'Cats wag their tails like dogs do to express happiness',
+        answer: false
+    },
+    {
+        question: 'Cats whiskers are just decorative and have no purpose',
+        answer: false
+    },
+    {
+        question: 'Some cats are ambidextous, but 40 percent are either left- or right-pawed',
+        answer: true  
+    }
+]
+function runQuiz {
+    
+}
